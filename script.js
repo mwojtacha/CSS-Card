ab676d63b325767289b8ab016cdffa2c43cc1542
mwojtacha <42960630+mwojtacha@users.noreply.github.com>
2020-10-21 10:42:44 +0200

    3 karty z możliwością rezerwacji

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const rezerwacja={
     text: document.getElementById("text"),
-    btn: document.getElementById("btn"),
+    btn: document.getElementById("btna"),
 
     btn1:function(){
-        this.text.innerHTML="ZAREZERWOWANO POMYŚLNIE";
+        this.text.innerHTML="ZAREZERWOWANO POMYŚLNIE WYCIECZKĘ DO KARPAT";
     },
 
     init:function(){
@@ -11,3 +11,31 @@ const rezerwacja={
     },
 }
 rezerwacja.init();
+
+const rezerwacja2={
+    text: document.getElementById("text"),
+    btn: document.getElementById("btnb"),
+
+    btn1:function(){
+        this.text.innerHTML="ZAREZERWOWANO POMYŚLNIE WYCIECZKĘ DO MALBORKA";
+    },
+
+    init:function(){
+        this.btn.addEventListener('click',this.btn1.bind(rezerwacja2));
+    },
+}
+rezerwacja2.init();
+
+const rezerwacja3={
+    text: document.getElementById("text"),
+    btn: document.getElementById("btnc"),
+
+    btn1:function(){
+        this.text.innerHTML="ZAREZERWOWANO POMYŚLNIE WYCIECZKĘ NAD MORZE";
+    },
+
+    init:function(){
+        this.btn.addEventListener('click',this.btn1.bind(rezerwacja3));
+    },
+}
+rezerwacja3.init();
