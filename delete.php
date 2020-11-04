a2390d6dce397275e160fd81512d0e60d0eadc9d
mwojtacha <42960630+mwojtacha@users.noreply.github.com>
2020-11-04 08:14:47 +0100

    Delete delete.php

--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php
- $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373162", "5YpM88aCen", "sql7373162");
- $id = $_POST['id'];
- $sql = "DELETE from lib_autor_tytul WHERE id_autor_tytul='$id'";
- mysqli_query($conn, $sql);
- header('Location: https://crud-michal-wojtacha.herokuapp.com');
-?>
